e929291370540a7ee1ac208c6c33ab197b24b609
Morris <109666488+junyuanM@users.noreply.github.com>
2024-06-01 16:34:22 +0800

    Update nvwa_downstream_pred.py

--- nvwa_downstream_pred.py
+++ nvwa_downstream_pred.py
@@ -3,7 +3,7 @@ import torch
 
 
 class BasicConv2d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, transpose=False, act_norm=False):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, transpose=False, act_norm=False, dropout=0.3):
         super(BasicConv2d, self).__init__()
         self.act_norm=act_norm
         if not transpose:
@@ -12,29 +12,30 @@ class BasicConv2d(nn.Module):
             self.conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding,output_padding=stride //2 )
         self.norm = nn.GroupNorm(2, out_channels)
         self.act = nn.LeakyReLU(0.2, inplace=True)
-
+        self.drop = nn.Dropout2d(dropout)
     def forward(self, x):
         y = self.conv(x)
         if self.act_norm:
             y = self.act(self.norm(y))
+        y = self.drop(y)
         return y
 
 
 class ConvSC(nn.Module):
-    def __init__(self, C_in, C_out, stride, transpose=False, act_norm=True):
+    def __init__(self, C_in, C_out, stride, transpose=False, act_norm=True, dropout=0.3):
         super(ConvSC, self).__init__()
         if stride == 1:
             transpose = False
         self.conv = BasicConv2d(C_in, C_out, kernel_size=3, stride=stride,
-                                padding=1, transpose=transpose, act_norm=act_norm)
+                                padding=1, transpose=transpose, act_norm=act_norm, dropout=dropout)
 
     def forward(self, x):
         y = self.conv(x)
         return y
 
 
-class GroupConv2d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, groups, act_norm=False):
+class GroupConv2d(nn.Module):# 分组卷积
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, groups, act_norm=False, dropout=0.3):
         super(GroupConv2d, self).__init__()
         self.act_norm = act_norm
         if in_channels % groups != 0:
@@ -42,21 +43,23 @@ class GroupConv2d(nn.Module):
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding,groups=groups)
         self.norm = nn.GroupNorm(groups,out_channels)
         self.activate = nn.LeakyReLU(0.2, inplace=True)
-    
+        self.drop = nn.Dropout2d(dropout)
+
     def forward(self, x):
         y = self.conv(x)
         if self.act_norm:
             y = self.activate(self.norm(y))
+        y = self.drop(y)
         return y
 
 
 class Inception(nn.Module):
-    def __init__(self, C_in, C_hid, C_out, incep_ker=[3,5,7,11], groups=8):        
+    def __init__(self, C_in, C_hid, C_out, incep_ker=[3,5,7,11], groups=8, dropout=0.3):        
         super(Inception, self).__init__()
         self.conv1 = nn.Conv2d(C_in, C_hid, kernel_size=1, stride=1, padding=0)
         layers = []
         for ker in incep_ker:
-            layers.append(GroupConv2d(C_hid, C_out, kernel_size=ker, stride=1, padding=ker//2, groups=groups, act_norm=True))
+            layers.append(GroupConv2d(C_hid, C_out, kernel_size=ker, stride=1, padding=ker//2, groups=groups, act_norm=True, dropout=dropout))
         self.layers = nn.Sequential(*layers)
 
     def forward(self, x):
@@ -73,12 +76,12 @@ def stride_generator(N, reverse=False):
     else: return strides[:N]
 
 class Encoder(nn.Module):
-    def __init__(self,C_in, C_hid, N_S):
+    def __init__(self,C_in, C_hid, N_S, dropout=0.3):
         super(Encoder,self).__init__()
         strides = stride_generator(N_S)
         self.enc = nn.Sequential(
-            ConvSC(C_in, C_hid, stride=strides[0]),
-            *[ConvSC(C_hid, C_hid, stride=s) for s in strides[1:]]
+            ConvSC(C_in, C_hid, stride=strides[0], dropout=dropout),
+            *[ConvSC(C_hid, C_hid, stride=s, dropout=dropout) for s in strides[1:]]
         )
     
     def forward(self,x):# B*4, 3, 128, 128
@@ -90,12 +93,12 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self,C_hid, C_out, N_S):
+    def __init__(self,C_hid, C_out, N_S, dropout=0.3):
         super(Decoder,self).__init__()
         strides = stride_generator(N_S, reverse=True)
         self.dec = nn.Sequential(
             *[ConvSC(C_hid, C_hid, stride=s, transpose=True) for s in strides[:-1]],
-            ConvSC(2*C_hid, C_hid, stride=strides[-1], transpose=True)
+            ConvSC(2*C_hid, C_hid, stride=strides[-1], transpose=True, dropout=dropout)
         )
         self.readout = nn.Conv2d(C_hid, C_out, 1)
     
@@ -105,21 +108,21 @@ class Decoder(nn.Module):
         Y = self.dec[-1](torch.cat([hid, enc1], dim=1))
         Y = self.readout(Y)
         return Y
-
+# 中间网络结构，用于对输入数据进行特征提取、融合和重建
 class Mid_Xnet(nn.Module):
-    def __init__(self, channel_in, channel_hid, N_T, incep_ker = [3,5,7,11], groups=8):
+    def __init__(self, channel_in, channel_hid, N_T, incep_ker = [3,5,7,11], groups=8, dropout=0.3):
         super(Mid_Xnet, self).__init__()
 
         self.N_T = N_T
-        enc_layers = [Inception(channel_in, channel_hid//2, channel_hid, incep_ker= incep_ker, groups=groups)]
+        enc_layers = [Inception(channel_in, channel_hid//2, channel_hid, incep_ker= incep_ker, groups=groups, dropout=dropout)]
         for i in range(1, N_T-1):
-            enc_layers.append(Inception(channel_hid, channel_hid//2, channel_hid, incep_ker= incep_ker, groups=groups))
-        enc_layers.append(Inception(channel_hid, channel_hid//2, channel_hid, incep_ker= incep_ker, groups=groups))
+            enc_layers.append(Inception(channel_hid, channel_hid//2, channel_hid, incep_ker= incep_ker, groups=groups, dropout=dropout))
+        enc_layers.append(Inception(channel_hid, channel_hid//2, channel_hid, incep_ker= incep_ker, groups=groups, dropout=dropout))
 
-        dec_layers = [Inception(channel_hid, channel_hid//2, channel_hid, incep_ker= incep_ker, groups=groups)]
+        dec_layers = [Inception(channel_hid, channel_hid//2, channel_hid, incep_ker= incep_ker, groups=groups, dropout=dropout)]
         for i in range(1, N_T-1):
-            dec_layers.append(Inception(2*channel_hid, channel_hid//2, channel_hid, incep_ker= incep_ker, groups=groups))
-        dec_layers.append(Inception(2*channel_hid, channel_hid//2, channel_in, incep_ker= incep_ker, groups=groups))
+            dec_layers.append(Inception(2*channel_hid, channel_hid//2, channel_hid, incep_ker= incep_ker, groups=groups, dropout=dropout))
+        dec_layers.append(Inception(2*channel_hid, channel_hid//2, channel_in, incep_ker= incep_ker, groups=groups, dropout=dropout))
 
         self.enc = nn.Sequential(*enc_layers)
         self.dec = nn.Sequential(*dec_layers)
@@ -139,19 +142,20 @@ class Mid_Xnet(nn.Module):
         # decoder
         z = self.dec[0](z)
         for i in range(1, self.N_T):
-            z = self.dec[i](torch.cat([z, skips[-i]], dim=1))
+            z = self.dec[i](torch.cat([z, skips[-i]], dim=1))# 沿着第二维度（通道维度）对张量 z 和 skips[-i] 进行拼接，将它们合并成一个新的张量
 
         y = z.reshape(B, T, C, H, W)
         return y
 
 
 class Nvwa_enchane_SimVP(nn.Module):
-    def __init__(self, shape_in, hid_S=16, hid_T=256, N_S=4, N_T=8, incep_ker=[3,5,7,11], groups=8):
+    def __init__(self, shape_in, hid_S=16, hid_T=256, N_S=4, N_T=8, incep_ker=[3,5], groups=8, dropout=0.3):
+    # def __init__(self, shape_in, hid_S=16, hid_T=256, N_S=4, N_T=8, incep_ker=[3,5,7,11], groups=8):
         super(Nvwa_enchane_SimVP, self).__init__()
         T, C, H, W = shape_in
-        self.enc = Encoder(C, hid_S, N_S)
-        self.hid = Mid_Xnet(T*hid_S, hid_T, N_T, incep_ker, groups)
-        self.dec = Decoder(hid_S, C, N_S)
+        self.enc = Encoder(C, hid_S, N_S, dropout)
+        self.hid = Mid_Xnet(T*hid_S, hid_T, N_T, incep_ker, groups, dropout)
+        self.dec = Decoder(hid_S, C, N_S, dropout)
 
 
     def forward(self, x_raw):
